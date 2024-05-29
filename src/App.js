77347623aa57a3b234a1e4b69b6b3a8f7833d006
Andres Järviste <andres.jarviste@wbscodingschool.com>
2024-05-29 19:17:17 +0300

    loeng5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         description: 'milk',
         amount: 1,
         unit: 'liter',
-        isInBasket: true,
+        isInBasket: false,
       },
       {
         description: 'apples',
@@ -36,13 +36,44 @@ function App() {
     setItems([...items, newProduct])
   }
 
+  const deleteProduct = (id) => {
+    // [1,2,3,4,5]
+    // [...[1,2], ... [4,5]] <- pick
+    // [1,2,null,4,5] <- filter
+
+    items[id] = null;
+
+    //Following filter functionis the same with:
+    // (el) => { return  el !== null }
+    let newItems = items.filter(el => el !== null )
+    setItems(newItems)
+  }
+
+  const toggleChecked = (id) => {
+    console.log('toggle checked for row ' + id)
+    const newItems = items.map((el, index) => {
+      if (index === id) {
+        return {...el, isInBasket: !el.isInBasket}
+      } else {
+        return el
+      }
+    })
+    setItems(newItems)
+  }
+
   return (
     <div className="container">
       <h1>My Shopping List</h1>
       <NewProduct addProduct = {addProduct} />
         {
           items.map((item, index) => {
-            return <Product key={index} product={item} />
+            return <Product 
+                      key={index} 
+                      product={item} 
+                      id={index} 
+                      deleteProduct={deleteProduct}
+                      toggleChecked={toggleChecked}
+                    />
           })
         }
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,25 @@
-function Product({product}) {
+function Product({product, id, deleteProduct, toggleChecked}) {
     let rowStyleClass = ""
     if (product.isInBasket) {
         rowStyleClass = "inBasket"
     }
+
+    const deleteClicked = () => {
+        console.log('Delete clicked on row ' + id);
+        //DO SOMETHING HERE TO ACTUALLY DELETE the DATA
+        deleteProduct(id)
+    }
+
     return (
         <div className= {"row " + rowStyleClass}>
+            <div className="col-1">
+                <input
+                    type="checkbox"
+                    defaultChecked={product.isInBasket}
+                    onChange={() => toggleChecked(id)}
+                />
+
+            </div>
             <div className="col-12 col-sm-6">
                 {product.description}
             </div>
@@ -13,6 +28,9 @@ function Product({product}) {
             </div>
             <div className="col-2">
                 {product.amount}
+            </div>
+            <div className="col-1">
+                <button onClick={deleteClicked} className="btn">Delete</button>
             </div>
         </div>
     )
